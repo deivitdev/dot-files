1dda2777a48c02625b8baa441e6707a054e7c172
David <saladavid0110@gmail.com>
2026-02-06 01:21:39 -0300

    fix(nvim): resolve grug-far keymap conflict and add todo search

--- nvim/init.lua
+++ nvim/init.lua
@@ -229,6 +229,13 @@ require('lazy').setup({
   { 'echasnovski/mini.surround', opts = {} },
   { 'echasnovski/mini.ai', version = false, opts = {} },
   {
+    'MagicDuck/grug-far.nvim',
+    opts = { headerMaxWidth = 80 },
+    keys = {
+      { '<leader>sr', function() require('grug-far').open() end, desc = 'Search and Replace (Grug)' },
+    },
+  },
+  {
     'folke/persistence.nvim',
     event = 'BufReadPre',
     opts = {},
@@ -525,7 +532,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sR', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
